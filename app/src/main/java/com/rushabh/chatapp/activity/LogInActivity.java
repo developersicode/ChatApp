436687fc7fb4ab989a5047331253a33b00bd38ca
DoshiRushabh <radoshi010@gmail.com>
2017-01-10 11:17:51 +0530

    Minor changes 2-1-2017

--- app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
+++ app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
@@ -4,19 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.rushabh.chatapp.MainActivity;
 import com.rushabh.chatapp.R;
 
 public class LogInActivity extends AppCompatActivity {
 
+    SignInButton signInButton;
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
 
 
 //        At last
-        startActivity(new Intent(LogInActivity.this,MainActivity.class));
+//        startActivity(new Intent(LogInActivity.this,MainActivity.class));
     }
 }
