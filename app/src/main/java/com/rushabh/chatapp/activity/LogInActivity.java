b4c7373225efdcce4a68fdb8a4b0e4a3c603a9fa
DoshiRushabh <radoshi010@gmail.com>
2017-01-11 17:04:55 +0530

    Google sign in added 2-1-2017

--- app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
+++ app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
@@ -1,19 +1,34 @@
 package com.rushabh.chatapp.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.rushabh.chatapp.MainActivity;
 import com.rushabh.chatapp.R;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-    SignInButton signInButton;
+    TextView name;
+    Button signOutBtn;
+    SignInButton signInBtn;
     private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +38,67 @@ public class LogInActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
+
+        name = (TextView) findViewById(R.id.name);
+
+        signInBtn = (SignInButton) findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(this);
+
+        signOutBtn = (Button) findViewById(R.id.signOutBtn);
+        signOutBtn.setOnClickListener(this);
 
 //        At last
 //        startActivity(new Intent(LogInActivity.this,MainActivity.class));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.signInBtn) {
+            signIn();
+        } else if (view.getId() == R.id.signOutBtn) {
+            signOut();
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            name.setText("Hello " + account.getDisplayName());
+        }
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                name.setText("Signed out");
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
