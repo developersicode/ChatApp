2467f78d741d5f4ddde73f0232fc5b5f84664f4e
DoshiRushabh <radoshi010@gmail.com>
2017-01-13 15:59:17 +0530

    Login added 2-1-2017

--- app/src/main/java/com/rushabh/chatapp/MainActivity.java
+++ app/src/main/java/com/rushabh/chatapp/MainActivity.java
@@ -1,16 +1,46 @@
 package com.rushabh.chatapp;
 
+import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.rushabh.chatapp.DAL.DAL;
+import com.rushabh.chatapp.activity.LogInActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+
+    Button logOut;
+    private GoogleApiClient mGoogleApiClient;
+    final DAL obj = new DAL(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logOut = (Button) findViewById(R.id.logOut);
+        logOut.setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
     private Boolean exit = false;
@@ -30,5 +60,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, 3 * 1000);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.logOut) {
+            signOut();
+        }
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+            }
+        });
+        obj.openDB();
+        obj.deleteData();
+        obj.closeDB();
+        startActivity(new Intent(MainActivity.this, LogInActivity.class));
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
+++ app/src/main/java/com/rushabh/chatapp/activity/LogInActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.rushabh.chatapp.DAL.DAL;
+import com.rushabh.chatapp.MainActivity;
 import com.rushabh.chatapp.R;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
@@ -32,7 +33,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     private GoogleApiClient mGoogleApiClient;
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+//    private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
     final DAL obj = new DAL(this);
 
@@ -58,11 +59,27 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         signInBtn = (SignInButton) findViewById(R.id.signInBtn);
         signInBtn.setOnClickListener(this);
 
-        signOutBtn = (Button) findViewById(R.id.signOutBtn);
-        signOutBtn.setOnClickListener(this);
+//        signOutBtn = (Button) findViewById(R.id.signOutBtn);
+//        signOutBtn.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
 
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null) {
+//                    // User is signed in
+//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+//                    Toast.makeText(LogInActivity.this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+//                } else {
+//                    // User is signed out
+//                    Log.d(TAG, "onAuthStateChanged:signed_out");
+//                    Toast.makeText(LogInActivity.this, "Loged out", Toast.LENGTH_SHORT).show();
+//                }
+//                // ...
+//            }
+//        };
 //        At last
 //        startActivity(new Intent(LogInActivity.this,MainActivity.class));
     }
@@ -73,7 +90,10 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         if (FullName == null) {
 
         } else {
-            name.setText("Hello" + FullName);
+//            name.setText("Hello" + FullName);
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            intent.putExtra("name",FullName);
+            startActivity(intent);
         }
         obj.closeDB();
     }
@@ -82,8 +102,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         if (view.getId() == R.id.signInBtn) {
             signIn();
-        } else if (view.getId() == R.id.signOutBtn) {
-            signOut();
+            Log.e("inside","On click");
         }
     }
 
@@ -91,54 +110,65 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        Log.e("inside","Sing in");
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.e("inside","onActivityResult");
 
         if (requestCode == RC_SIGN_IN) {
+            Log.e("inside","If statment");
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
     }
 
     private void handleSignInResult(GoogleSignInResult result) {
+        Log.e("inside","handleSignInResult");
         if (result.isSuccess()) {
             GoogleSignInAccount account = result.getSignInAccount();
-            name.setText("Hello " + account.getDisplayName());
+//            name.setText("Hello " + account.getDisplayName());
             obj.openDB();
             obj.insertData(account.getDisplayName());
             obj.closeDB();
+            Log.e("inside","isSuccess");
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            intent.putExtra("name",account.getDisplayName());
+            startActivity(intent);
+        }else {
+            Log.e("inside","failed");
+            Log.e("status"," " + result.getStatus());
         }
     }
 
-    private void signOut() {
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                name.setText("Signed out");
-            }
-        });
-        obj.openDB();
-        obj.deleteData();
-        obj.closeDB();
-    }
+//    private void signOut() {
+//        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+//            @Override
+//            public void onResult(@NonNull Status status) {
+////                name.setText("Signed out");
+//            }
+//        });
+//        obj.openDB();
+//        obj.deleteData();
+//        obj.closeDB();
+//    }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mAuth.removeAuthStateListener(mAuthListener);
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        mAuth.removeAuthStateListener(mAuthListener);
+//    }
 }
